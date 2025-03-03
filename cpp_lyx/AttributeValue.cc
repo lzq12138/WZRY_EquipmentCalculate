9ed1dfdda2e2e36e35a5a8cea4a4cfabb9a0f4da
“luren” <liyunxiang126@126.com>
2025-03-03 23:12:08 +0800

    Add percentage value

--- cpp_lyx/AttributeValue.cc
+++ cpp_lyx/AttributeValue.cc
@@ -1,9 +1,20 @@
 #include "AttributeValue.h"
 
-AttributeValue::AttributeValue(double value1=0.0, double value2=0.0){
-        m_value = value1;
-        m_extraValue = value2;
-        
+AttributeValue::AttributeValue(std::string name="", double value1=0.0, double value2=0.0, double value3=0.0){
+    m_name = name;
+    m_value = value1;
+    m_extraValue = value2;
+    m_percentageValue = value3;
+}
+
+std::string
+AttributeValue::get_name(){
+    return m_name;
+}
+
+void
+AttributeValue::set_name(std::string name){
+    m_name = name;
 }
 
 double
@@ -24,4 +35,14 @@ AttributeValue::get_extraValue(){
 void
 AttributeValue::set_extraValue(double value){
     m_extraValue = value;
+}
+
+double
+AttributeValue::get_percentageValue(){
+    return m_percentageValue;
+}
+
+void
+AttributeValue::set_persentageValue(double value){
+    m_percentageValue = value;
 }
--- cpp_lyx/AttributeValue.h
+++ cpp_lyx/AttributeValue.h
@@ -1,15 +1,25 @@
+#include <string>
+// using namespace std;
+
+// Class for one attribute
 class AttributeValue{
 
     public:
-    AttributeValue(double value1=0.0, double value2=0.0);
+    AttributeValue(std::string name="", double value1=0.0, double value2=0.0, double value3=0.0);
 
+    std::string get_name();
+    void set_name(std::string name);
     double get_Value();
     void set_Value(double value);
     double get_extraValue();
     void set_extraValue(double value);
+    double get_percentageValue();
+    void set_persentageValue(double value);
 
     private:
-    double m_value;
-    double m_extraValue;
+    std::string m_name;         // name of atteibute
+    double m_value;             // basic value
+    double m_extraValue;        // extra value
+    double m_percentageValue;   // percentage value
 
 };
