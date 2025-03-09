94ee97f88b64a7283a34f75d8ddbff5547c9ade2
“luren” <liyunxiang126@126.com>
2025-03-09 14:15:00 +0800

    Add set_name and get_attribute_byname method

--- cpp_lyx/HeroModelBase.cc
+++ cpp_lyx/HeroModelBase.cc
@@ -7,6 +7,16 @@ HeroModelBase::HeroModelBase(std::string name, int attr_num=18){
 }
 
 void
+HeroModelBase::set_name(std::string name){
+    m_name = name;
+}
+
+std::string
+HeroModelBase::get_name(){
+    return m_name;
+}
+
+void
 HeroModelBase::set_AttackRange(std::string AR){
     m_AttackRange = AR;
 }
@@ -19,4 +29,14 @@ HeroModelBase::get_AttackRange(){
 AttributeValue
 HeroModelBase::get_Attribute(int attr){
     return m_attr_list[attr];
+}
+
+AttributeValue
+HeroModelBase::get_Attribute_byname(std::string attr_name){
+    for(int i=0; i<m_attr_num; i++){
+        if(m_attr_list[i].get_name() == attr_name){
+            return m_attr_list[i];
+        }
+    }
+    throw std::invalid_argument("Attribute not found");
 }
--- cpp_lyx/HeroModelBase.h
+++ cpp_lyx/HeroModelBase.h
@@ -5,11 +5,22 @@
 class HeroModelBase{
     public:
     HeroModelBase(std::string name, int attr_num=18);
-        
+
+    // Set Hero Name
+    void set_name(std::string name);
+    // Get Hero Name
+    std::string get_name();
+    
+    // Set Attack Range
     void set_AttackRange(std::string AR);
+    // Get Attack Range
     std::string get_AttackRange();
 
+    // get Attribute by index
     AttributeValue get_Attribute(int attr);
+
+    // get Attribute by name
+    AttributeValue get_Attribute_byname(std::string attr_name);
 
     private:
 
