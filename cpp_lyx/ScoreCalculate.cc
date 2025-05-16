70a25b16db2b8f929bd12c174904ca5253eb1c10
“luren” <liyunxiang126@126.com>
2025-05-16 23:04:21 +0800

    make static vars const

--- cpp_lyx/ScoreCalculate.cc
+++ cpp_lyx/ScoreCalculate.cc
@@ -20,8 +20,8 @@ double attribute_score(AttributeValue attr){
     return base_factor * base_value + percentage_factor * percentage_value;
 }
 
-static double base_attribute_factor[N_ATTR] = {HP_FACTOR, MP_FACTOR, AD_FACTOR, AP_FACTOR, PD_FACTOR, MD_FACTOR, MS_FACTOR, PPen_FACTOR, MPen_FACTOR, AS_FACTOR, CR_FACTOR, CD_FACTOR, LS_FACTOR, SV_FACTOR, CDR_FACTOR, Tenacity_FACTOR, HReg_FACTOR, MReg_FACTOR};
-static double extra_attribute_factor[N_ATTR] = {HP_FACTOR2, MP_FACTOR2, AD_FACTOR2, AP_FACTOR2, PD_FACTOR2, MD_FACTOR2, MS_FACTOR2, PPen_FACTOR2, MPen_FACTOR2, AS_FACTOR2, CR_FACTOR2, CD_FACTOR2, LS_FACTOR2, SV_FACTOR2, CDR_FACTOR2, Tenacity_FACTOR2, HReg_FACTOR2, MReg_FACTOR2};
+static const double base_attribute_factor[N_ATTR] = {HP_FACTOR, MP_FACTOR, AD_FACTOR, AP_FACTOR, PD_FACTOR, MD_FACTOR, MS_FACTOR, PPen_FACTOR, MPen_FACTOR, AS_FACTOR, CR_FACTOR, CD_FACTOR, LS_FACTOR, SV_FACTOR, CDR_FACTOR, Tenacity_FACTOR, HReg_FACTOR, MReg_FACTOR};
+static const double extra_attribute_factor[N_ATTR] = {HP_FACTOR2, MP_FACTOR2, AD_FACTOR2, AP_FACTOR2, PD_FACTOR2, MD_FACTOR2, MS_FACTOR2, PPen_FACTOR2, MPen_FACTOR2, AS_FACTOR2, CR_FACTOR2, CD_FACTOR2, LS_FACTOR2, SV_FACTOR2, CDR_FACTOR2, Tenacity_FACTOR2, HReg_FACTOR2, MReg_FACTOR2};
 
 
 double base_factor_from_idx(int idx){
